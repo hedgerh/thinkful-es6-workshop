5779d076e744937540d4b73ac42d49b5f5d50937
John McCaffrey <john.mccaffrey@gmail.com>
2015-10-12 22:09:16 +0000

    changes to make it easier to run on Cloud9

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+//var host = 'localhost'
+// var port = '3000'
+
+//will help to run it on cloud9 (c9.io)
+var host = process.env.IP ; 
+var port = process.env.PORT ; 
+
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, host, function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:3000');
+  console.log('Listening at ' + host + port);
 });
